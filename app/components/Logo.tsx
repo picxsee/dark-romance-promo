ae3ea314035ee749623ccbc7befbb1e13e853560
PICXSEE <264004528+picxsee@users.noreply.github.com>
2026-09-09 23:17:44 +0200

    Delete app/components directory

--- app/components/Logo.tsx
+++
@@ -1,8 +0,0 @@
-export default function Logo() {
-  return (
-    <svg width="42" height="42" viewBox="0 0 42 42" fill="none">
-      <rect x="1" y="1" width="40" height="40" rx="13" fill="#120D20" stroke="#A66CFF" strokeWidth="1.5" />
-      <path d="M12 29V13h6.4c5.4 0 8.6 2.7 8.6 7.7 0 5.3-3.4 8.3-9 8.3H12Zm4-3.2h2c3.3 0 5.2-1.7 5.2-5.1 0-3.2-1.9-4.5-5-4.5H16v9.6Z" fill="#C49AFF" />
-    </svg>
-  );
-}
